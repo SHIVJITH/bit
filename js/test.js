bfec421e369ff09f9040afa1f459bd33cf9b581f
Sabainath Rajendran <sabarinathkrajenderan@gmail.com>
2020-12-29 00:29:42 +0530

    added route

--- js/test.js
+++ js/test.js
@@ -4,7 +4,14 @@ const direction={
     three:{icon:"forward", text:"Walk Straight 50Mtrs", isDone :false},
     four:{icon:"right", text:"To your right is Boys Hostel", isDone :false}
 }
-
+const mListElement = document.querySelector("#myUL");
+const mapprdistance = document.querySelector("#distance");
 for(let val in direction){
         console.log(direction[val]);
+    const listElement = document.createElement("li");
+  
+      listElement.innerHTML = direction[val].text;
+  
+      mListElement.appendChild(listElement);
 }
+mapprdistance.innerHTML = mapprdistance.innerHTML +" " + "400 Mtrs";
